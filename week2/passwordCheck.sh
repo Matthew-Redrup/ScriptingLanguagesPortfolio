49286e200f178fc9456fd9e831ab6e1c6157d8ea
Matthew Redrup <matthew.redrup@gmail.com>
2020-06-20 22:14:02 -0400

    passwordCheck.sh final add

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-
+# Ask the user to type a secret password (hide that input)
 read -s -p 'Type a secret password: ' secretPassword # take password input from user
 
 echo $secretPassword | sha256sum > secret.sha256 # hash the input from the user and store in file
 mv secret.sha256 test
 cd test
+# Check the user's password against the hash stored in secret.txt
 PasswordHash=secret.txt
 PasswordTry=secret.sha256
 if cmp -s "$PasswordHash" "$PasswordTry"; then
-	echo "Access granted"
+	echo "Access granted"# If the users password is correct, print "Access Granted" and quit with the exit code 0
 	exit 0
 else
-	echo "Access denied"
+	echo "Access denied"# If the users password is incorrect print "Access Denied" and quit with the exit code 1.
 	exit 1
 fi
-
-# Ask the user to type a secret password (hide that input)
-
-# Check the user's password against the hash stored in secret.txt
-
-# If the users password is correct, print "Access Granted" and quit with the exit code 0.
-
-# If the users password is incorrect print "Access Denied" and quit with the exit code 1.
-
-
